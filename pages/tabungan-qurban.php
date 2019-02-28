b06aec467a69da0acc5a84767e7fb7e3354b585c
woyman1 <woyman11@gmail.com>
2019-02-28 17:41:36 +0700

    fix beberapa halaman produk

--- pages/tabungan-qurban.php
+++ pages/tabungan-qurban.php
@@ -26,47 +26,49 @@
 	
 </section>
 
-
-
-
 <section id="maincontent">
     <div class="container">
       <div class="row">
-        <div class="span9">
-		<h4>Pengertian </h4>
-<p>Tabungan Qurban adalah merupakan salah satu produk tabungan KSPPS BMT PAS yang membantu  anggota/nasabah mewujudkan rencana qur’bannya di bulan idul adha.
-Dalil Al-qur’an : “Maka laksanakanlah sholat karena Tuhanmu, dan berqurbanlah (sebagai ibadah dan mendekatkan diri kepada Allah).” 
-(Q.S. al-Kautsar: 2).</p><p>
-Hadist A’isyah R.a, Rasulullah SAW  bersabda, “Sabaik-baik amal bani adam bagi Allah di hari Idul Adha  adalah menyembelih qurban. Pada hari kiamat hewan-hewan qurban tersebut menyertai bani adam dengan tanduk-tanduknya, tulang-tulang dan bulunya,  darah hewan tersebut diterima oleh Allah sebelum menetes ke bumi dan akan membersihkan mereka yang melakukannya”. 
-(H.R. Tirmizi, Ibnu Majah)</p><ol>
-<h5>Apa Keuntungan bagi Anggota/ Nasabah?</h5>
-<li>	Memudahkan anggota/Nasabah mewujudkan niat suci untuk berqurban dengan mudah</li>
-<li>	Setoran bisa sewaktu-waktu dan bisa dilayani jemput bola </li>
-<li>	Mendapatkan bonus biaya penyembelihan </li>
-<li>	Saldo tabungan bisa digunakan untuk agunan/jaminan pembiayaan</li>
-<li>Tidak ada biaya administrasi bulanan.</li></ol>
-<h5>	Apa saja syarat dan ketentuan membuka rekening Tabungan Qurban?</h5>
-<ol>
-<li>Mendaftarkan/terdaftar sebagai anggota BMT PAS</li>
-<li>Menyerahkan Fotokopi KTP/KK</li>
-<li>Menandatangani surat pernyataan tentang hak dan kewajiban Program Tabungan Qurban</li>
-<li>Menyetorkan Pembukaan Rekening dengan Saldo minimal Rp. 100.000-,</li>
-<li>Penyetoran selanjutnya bisa dilakukan sewaktu-waktu minimal Rp. 50.000-, </li>
-<li>Penyetoran bisa ke kantor BMT PAS dan atau hubungi marketing kami untuk “jemput bola”</li>
-<li>Penarikan Saldo Tabungan Qurban hanya bisa dilakukan paling cepat 5 hari sebelum idul adha.</li></ol>
 
+      <div class="span3">
+          <aside>
+            <div class="widget">
+              <h4>Produk & Layanan Lainnya</h4>
+              <ul class="cat">
+                <li><a href="#">Tabungan Simbah</a></li>
+                <li><a href="#">Pembiayaan</a></li>
+              </ul>
+            </div>
+          </aside>
+        </div>
 
+        <div class="span9" style="margin-bottom: 60px">
+      		<h4>Apa itu Tabungan Qurban? </h4>
+              <p>Tabungan Qurban adalah merupakan salah satu produk tabungan KSPPS BMT PAS yang membantu  anggota/nasabah mewujudkan rencana qur’bannya di bulan idul adha.</p>
 
-         
+              <p>Dalil Al-qur’an : “Maka laksanakanlah sholat karena Tuhanmu, dan berqurbanlah (sebagai ibadah dan mendekatkan diri kepada Allah).” 
+              (Q.S. al-Kautsar: 2).</p>
+              <p>Hadist A’isyah R.a, Rasulullah SAW  bersabda, “Sabaik-baik amal bani adam bagi Allah di hari Idul Adha  adalah menyembelih qurban. Pada hari kiamat hewan-hewan qurban tersebut menyertai bani adam dengan tanduk-tanduknya, tulang-tulang dan bulunya,  darah hewan tersebut diterima oleh Allah sebelum menetes ke bumi dan akan membersihkan mereka yang melakukannya”. 
+              (H.R. Tirmizi, Ibnu Majah)</p>
+            <h5>Apa Keuntungan bagi Anggota/ Nasabah?</h5>
+              <ol>
+                <li>	Memudahkan anggota/Nasabah mewujudkan niat suci untuk berqurban dengan mudah</li>
+                <li>	Setoran bisa sewaktu-waktu dan bisa dilayani jemput bola </li>
+                <li>	Mendapatkan bonus biaya penyembelihan </li>
+                <li>	Saldo tabungan bisa digunakan untuk agunan/jaminan pembiayaan</li>
+                <li>Tidak ada biaya administrasi bulanan.</li>
+              </ol>
+              <h5>Apa saja syarat dan ketentuan membuka rekening Tabungan Qurban?</h5>
+              <ol>
+                <li>Mendaftarkan/terdaftar sebagai anggota BMT PAS</li>
+                <li>Menyerahkan Fotokopi KTP/KK</li>
+                <li>Menandatangani surat pernyataan tentang hak dan kewajiban Program Tabungan Qurban</li>
+                <li>Menyetorkan Pembukaan Rekening dengan Saldo minimal Rp. 100.000-,</li>
+                <li>Penyetoran selanjutnya bisa dilakukan sewaktu-waktu minimal Rp. 50.000-, </li>
+                <li>Penyetoran bisa ke kantor BMT PAS dan atau hubungi marketing kami untuk “jemput bola”</li>
+                <li>Penarikan Saldo Tabungan Qurban hanya bisa dilakukan paling cepat 5 hari sebelum idul adha.</li>
+              </ol>
 
-        
-			   
-			   
-            
-		  
-		  
-           
-        
       </div>
     </div>
   </section>
