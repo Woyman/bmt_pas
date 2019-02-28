07058b39f60ffe8755c1317351aba7d481b2c88b
16110272 <dwisaputratkj2@gmail.com>
2019-02-28 17:56:49 +0700

    tabungan-thr

--- pages/tabungan-thr.php
+++ pages/tabungan-thr.php
@@ -18,7 +18,7 @@
         <div class="span12">
           <ul class="breadcrumb notop">
             <li><a href="index.php">BMT PAS </a><span class="divider">/</span></li>
-            <li class="active">Tabungan qurban BMT Projo Artha Sejahtera</li>
+            <li class="active">TABUNGAN HARI RAYA (THR)BMT Projo Artha Sejahtera</li>
           </ul>
         </div>
       </div>
@@ -43,31 +43,34 @@
         </div>
 
         <div class="span9" style="margin-bottom: 60px">
-        <h4>Pengertian </h4>
-        <p>Tabungan Qurban adalah merupakan salah satu produk tabungan KSPPS BMT PAS yang membantu  anggota/nasabah mewujudkan rencana qur’bannya di bulan idul adha.</p>
+        <h4>Apa itu Tabungan THR ?</h4>
+        <p>THR adalah merupakan salah satu produk tabungan KSPPS BMT PAS yang membantu  anggota/nasabah mewujudkan kebahagiaan di hari raya iedul fitri.</p>
+<p>Dalil Al-qur’an : "Dan janganlah kamu jadikan tanganmu terbelenggu pada lehermu (pelit) dan janganlah kamu terlalu mengulurkannya (boros) karena itu kamu menjadi tercela dan menyesal. “QS. Al Isra' (17) ayat 29 </p>
+<p>Hadist Rasulullah SAW  bersabda, “Simpanlah sebagian dari harta kamu untuk kebaikan masa depan kamu, karena itu jauh lebih baik bagimu.” (Hadits Riwayat Bukhari)</p>
 
-        <p>Dalil Al-qur’an : “Maka laksanakanlah sholat karena Tuhanmu, dan berqurbanlah (sebagai ibadah dan mendekatkan diri kepada Allah).” (Q.S. al-Kautsar: 2).</p>
-        <p>Hadist A’isyah R.a, Rasulullah SAW  bersabda, “Sabaik-baik amal bani adam bagi Allah di hari Idul Adha  adalah menyembelih qurban. Pada hari kiamat hewan-hewan qurban tersebut menyertai bani adam dengan tanduk-tanduknya, tulang-tulang dan bulunya,  darah hewan tersebut diterima oleh Allah sebelum menetes ke bumi dan akan membersihkan mereka yang melakukannya”. 
-        (H.R. Tirmizi, Ibnu Majah)</p>
+        
 
         <h5>Apa Keuntungan bagi Anggota/ Nasabah?</h5>
 
         <ol>        
-          <li>	Memudahkan anggota/Nasabah mewujudkan niat suci untuk berqurban dengan mudah</li>
-          <li>	Setoran bisa sewaktu-waktu dan bisa dilayani jemput bola </li>
-          <li>	Mendapatkan bonus biaya penyembelihan </li>
-          <li>	Saldo tabungan bisa digunakan untuk agunan/jaminan pembiayaan</li>
-          <li>Tidak ada biaya administrasi bulanan.</li>
+         <li>Memudahkan anggota/Nasabah mewujudkan kebahagiaan di hari raya idul fitri dengan mudah</li>
+		<li>Setoran bisa sewaktu-waktu dan bisa dilayani jemput bola </li>
+		<li>Mendapatkan bonus parcel lebaran dengan saldo tertentu*</li>
+		<li>Saldo tabungan bisa digunakan untuk agunan/jaminan pembiayaan </li>
+		<li>Tidak ada biaya administrasi bulanan.</li>
+		<li>mendapatkan tambahan bagi hasil bulanan yang kompetitif.</li>
+
         </ol>
-        <h5>	Apa saja syarat dan ketentuan membuka rekening Tabungan Qurban?</h5>
+        <h5>	Apa saja syarat dan ketentuan membuka rekening Tabungan THR?</h5>
         <ol>
-          <li>Mendaftarkan/terdaftar sebagai anggota BMT PAS</li>
-          <li>Menyerahkan Fotokopi KTP/KK</li>
-          <li>Menandatangani surat pernyataan tentang hak dan kewajiban Program Tabungan Qurban</li>
-          <li>Menyetorkan Pembukaan Rekening dengan Saldo minimal Rp. 100.000-,</li>
-          <li>Penyetoran selanjutnya bisa dilakukan sewaktu-waktu minimal Rp. 50.000-, </li>
-          <li>Penyetoran bisa ke kantor BMT PAS dan atau hubungi marketing kami untuk “jemput bola”</li>
-          <li>Penarikan Saldo Tabungan Qurban hanya bisa dilakukan paling cepat 5 hari sebelum idul adha.</li>
+         
+<li>Mendaftarkan/terdaftar sebagai anggota BMT PAS</li>
+<li>Menyerahkan Fotokopi KTP/KK</li>
+<li>Menandatangani surat pernyataan tentang hak dan kewajiban Program Tabungan THR</li>
+<li>Menyetorkan Pembukaan Rekening dengan Saldo minimal Rp. 100.000-,</li>
+<li>Penyetoran selanjutnya bisa dilakukan sewaktu-waktu minimal Rp. 50.000-, </li>
+<li>Penyetoran bisa ke kantor BMT PAS dan atau hubungi marketing kami untuk “jemput bola”</li>
+<li>Penarikan Saldo Tabungan THR hanya bisa dilakukan paling cepat 5 hari sebelum idul fitri.</li>
         </ol>
 
       </div>
