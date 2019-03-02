cfd564a11295dd0c2acf81583044ff18f60fb903
16110272 <dwisaputratkj2@gmail.com>
2019-03-02 13:29:58 +0700

    tabungan SMART

---
+++ pages/tabungan-smart.php
@@ -0,0 +1,112 @@
+<section id="subintro">
+    <div class="jumbotron subhead" id="overview">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="centered">
+
+              <h3>Tabungan SMART BMT PAS</h3>
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+<section id="breadcrumb">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <ul class="breadcrumb notop">
+            <li><a href="index.php">BMT PAS </a><span class="divider">/</span></li>
+
+            <li class="active">Tabungan SMART BMT Projo Artha Sejahtera</li>
+          </ul>
+        </div>
+      </div>  
+
+    </div>
+	
+</section>
+
+
+
+
+
+<section id="maincontent">
+    <div class="container">
+      <div class="row">
+
+      <div class="span3">
+          <aside>
+            <div class="widget">
+              <h4>Produk & Layanan Lainnya</h4>
+              <ul class="cat">
+                    <li><a href="index.php?page=tabungan-saham">Tabungan Saham</a></li>
+                    <li><a href="index.php?page=tabungan-qurban">Tabungan Qurban</a></li>
+                    <li><a href="index.php?page=tabungan-thr">Tabungan THR</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Tasbih</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Simbah</a></li>
+                    <li><a href="index.php?page=pembiayaan-simbah">Pembiayaan Simbah</a></li>
+					<li><a href="index.php?page=tabungan-ibadah">Tabungan Simbah</a></li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+
+        <div class="span9" style="margin-bottom: 60px">
+      		<h4>Apa itu Tabungan IBADAH? </h4>
+              <p>Tabungan SMART adalah simpanan akhir tahun, merupakan salah satu produk tabungan 
+			  KSPPS BMT PAS yang dikerjasamakan dengan lembaga pendidikan/sekolah 
+			  dalam rangka membantu  siswa-siswi  belajar untuk berbudaya menabung.
+			</p>
+
+              <p>	Dalil Al-qur’an : "Dan janganlah kamu jadikan tanganmu terbelenggu pada lehermu (pelit) dan janganlah kamu terlalu mengulurkannya (boros) karena itu kamu menjadi tercela dan menyesal. “QS. Al Isra' (17) ayat 29 </p>
+
+              <p>	Hadist Rasulullah SAW  bersabda, “Simpanlah sebagian dari harta kamu untuk kebaikan masa depan kamu, karena itu jauh lebih baik bagimu.” (Hadits Riwayat Bukhari)
+
+</p>
+            <h5>Apa Keuntungan bagi Anggota/ Nasabah?</h5>
+              <ol>
+                <li>	Mendidik siswa siswi belajar untuk berbudaya menabung</li>
+ <li>Setoran bisa sewaktu- waktu dan bisa dilayani jemput bola </li>
+ <li>Saldo tabungan bisa digunakan untuk agunan/jaminan pembiayaan orang tua wali siswa</li>
+ <li>	Tidak ada biaya administrasi bulanan.</li>
+ <li>Mendapatkan tambahan bagi hasil bulanan yang kompetitif</li>
+ <li>	Bagi siswa yang paling rajin menabung mendapatkan hadiah</li>
+ <li>KSPPS BMT PAS turut serta dalam sponshorship kegiatan Lembaga sekolah tersebut
+
+</li>
+
+              </ol>
+              <h5>Apa saja syarat dan ketentuan membuka rekening Tabungan SMART?</h5>
+              <ol>
+                <li>Mendaftarkan/terdaftar sebagai anggota BMT PAS dibawah perwalian orang tua</li>
+<li>Menyerahkan Fotokopi KK dan KTP orang tua wali</li>
+<li>	Menandatangani surat pernyataan tentang hak dan kewajiban Program SMART</li>
+<li>Menyetorkan Pembukaan Rekening dengan Saldo minimal Rp. 20.000-,</li>
+<li>	Peyetoran saldo berikutnya minimal Rp. 10.000-,</li>
+<li>Penyetoran bisa dilayani di lembaga pendidikan yang sudah kerjasama</li>
+<li>Penarikan Saldo Tabungan SMART dilakuakan pada saat kenaikan kelas/ ganti tahun ajaran
+</li>
+              </ol>
+
+              <h5>Table Tabungan IBADAH</h5>
+              <img src="assets/img/table-tabungan-simbah.png" width="700">
+              <br><br>
+
+              <strong>
+              <h6>Catatan :</h6>
+              <ol>
+                <li>Nilai Bagi Hasil tiap bulan di atas hanya gambaran nilai rata-rata pembulatan 
+	salama 5 tahun terakhir di KSPPS BMT PAS
+</li>
+               
+              </ol>
+              </strong>
+
+
+      </div>
+    </div>
+  </section>
