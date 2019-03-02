d58425cc921de75092d27716840304ab0f842d34
16110272 <dwisaputratkj2@gmail.com>
2019-03-02 17:12:53 +0700

    pembiayaan multiguna

---
+++ pages/pembiayaan-multiguna.php
@@ -0,0 +1,85 @@
+<section id="subintro">
+    <div class="jumbotron subhead" id="overview">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="centered">
+               <h3>Pembiayaan Multi Guna BMT PAS</h3>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+<section id="breadcrumb">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <ul class="breadcrumb notop">
+            <li><a href="index.php">BMT PAS </a><span class="divider">/</span></li>
+            <li class="active">Pembiayaan Multi Guna BMT Projo Artha Sejahtera</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+	
+</section>
+
+<section id="maincontent">
+    <div class="container">
+      <div class="row">
+
+      <div class="span3">
+          <aside>
+            <div class="widget">
+              <h4>Produk & Layanan Lainnya</h4>
+              <ul class="cat">
+                    <li><a href="index.php?page=tabungan-saham">Tabungan Saham</a></li>
+                    <li><a href="index.php?page=tabungan-qurban">Tabungan Qurban</a></li>
+                    <li><a href="index.php?page=tabungan-thr">Tabungan THR</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Tasbih</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Simbah</a></li>
+                    <li><a href="index.php?page=pembiayaan-simbah">Pembiayaan Simbah</a></li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+
+        <div class="span9" style="margin-bottom: 60px">
+      		<h4>Apa itu Pembiayaan Multi Guna? </h4>
+              <p>Merupakan pembiayaan untuk memfasilitasi anggota/nasabah dalam memenuhi kebutuhan jasa keperluan non produktif dengan jangka waktu maksimal sampai dengan 36 bulan.</p>
+
+             
+            <h5>Jenis Pembiayan yang di layani :</h5>
+              <ol>
+<li>Biaya Sekolah	</li>
+<li>Biaya Renovasi Rumah</li>
+<li>Biaya Slametan	</li>
+<li>Biaya Mantenan</li>
+<li>Biaya Sunatan	</li>
+<li>Biaya Rumah Sakit
+</li>
+              </ol>
+              <h5>Ketentuan Pembiayaan Multi Guna :</h5>
+              <ol>
+               <li>Peruntukan/ kegunaan dana BMT PAS adalah untuk jasa keperluan non produktif</li>
+<li>Skema Angsuran pembiayaan adalah dengan angsuran pokok dan Ujroh/imbalan jasa atau fee yang dibayar setiap bulan </li>
+<li>Besarnya ujroh/imbalan jasa atau fee bisa di lihat di ilustrasi table angsuran</li>
+<li>Jangka Waktu untuk angsuran maximal 36 bulan
+
+
+</li>
+              </ol>
+<h5>	Syarat Pembiayaan Multi Guna :</h5>
+<ol><li>FC KTP Pemohon, suami /istri pemohon</li>
+<li>FC KK dan Akta nikah</li>
+<li>FC Agunan  Sertifikat atau BPKB (Dokumen asli Sertifikat atau BPKB diserahkan pada saat realisasi pembiayaan)</li>
+<li>FC PBB berikut STTSnya jika agunan tanah bangunan, jika kendaraan dilengkapi dengan gesekan nomor mesin & nomor rangka melalui cek bantu Polres/samsat</li>
+<li>FC SIUP, TDP, HO (jika diperlukan)
+</li>
+
+
+      </div>
+    </div>
+  </section>
