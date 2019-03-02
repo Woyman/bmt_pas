9b20a845732026ca26314662c5e590844d6d20f4
16110272 <dwisaputratkj2@gmail.com>
2019-03-02 15:41:57 +0700

    beberapa pembiayaan

---
+++ pages/pembiayaan-konsumtif.php
@@ -0,0 +1,89 @@
+<section id="subintro">
+    <div class="jumbotron subhead" id="overview">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="centered">
+               <h3>Pembiayaan Konsumtif BMT PAS</h3>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+<section id="breadcrumb">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <ul class="breadcrumb notop">
+            <li><a href="index.php">BMT PAS </a><span class="divider">/</span></li>
+            <li class="active">Pembiayaan Konsumtif BMT Projo Artha Sejahtera</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+	
+</section>
+
+<section id="maincontent">
+    <div class="container">
+      <div class="row">
+
+      <div class="span3">
+          <aside>
+            <div class="widget">
+              <h4>Produk & Layanan Lainnya</h4>
+              <ul class="cat">
+                    <li><a href="index.php?page=tabungan-saham">Tabungan Saham</a></li>
+                    <li><a href="index.php?page=tabungan-qurban">Tabungan Qurban</a></li>
+                    <li><a href="index.php?page=tabungan-thr">Tabungan THR</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Tasbih</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Simbah</a></li>
+                    <li><a href="index.php?page=pembiayaan-simbah">Pembiayaan Simbah</a></li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+
+        <div class="span9" style="margin-bottom: 60px">
+      		<h4>Apa itu Pembiayaan Konsumtif? </h4>
+              <p>Merupakan pembiayaan untuk memfasilitasi anggota/nasabah dalam memenuhi kebutuhan kepemilikan kendaraan, elektronik, rumah, tanah, perhiasan, dan asset sejenis dengan jangka waktu maksimal sampai dengan 36 bulan dengan akad murabahah.</p>
+
+             
+            <h5>Jenis Pembiayan yang di layani :</h5>
+              <ol>
+               
+<li>Pembelian Mobil	</li>
+<li>Pembelian HaPe</li>
+<li>Pembelian Motor</li>
+<li>Pembelian Televisi</li>
+<li>Pembelian Sepeda</li>	
+<li>Pembelian AC</li>
+<li>Pembelian Rumah	</li>
+<li>Pembelian Laptop/camera</li>
+<li>Pembelian Perhiasan	</li>
+<li>Pembelian Kulkas
+
+</li>
+              </ol>
+              <h5>Ketentuan Pembiayaan Konsumtif :</h5>
+              <ol>
+               <li>Peruntukan/ kegunaan dana BMT PAS adalah untuk kepemilikan asset</li>
+<li>	Angsuran Pembiayaan dengan angsuran pokok dan margin keuntungan  yang dibayar setiap bulan </li>
+<li>Besarnya Margin keuntungan bisa di lihat di ilustrasi table angsuran</li>
+<li>	Jangka Waktu untuk angsuran maximal 36 bulan
+
+</li>
+              </ol>
+<h5>	Syarat Pembiayaan Konsumtif :</h5>
+<ol><li>FC KTP Pemohon, suami /istri pemohon</li>
+<li>	FC KK dan Akta nikah</li>
+<li>FC Agunan  Sertifikat atau BPKB (Dokumen asli Sertifikat atau BPKB diserahkan pada saat realisasi pembiayaan)</li>
+<li>	FC PBB berikut STTSnya jika agunan tanah bangunan, jika kendaraan dilengkapi dengan gesekan nomor mesin & nomor rangka melalui cek bantu Polres/samsat</li>
+<li>	FC SIUP, TDP, HO (jika diperlukan)</li>
+
+
+      </div>
+    </div>
+  </section>
---
+++ pages/pembiayaan-produktif.php
@@ -0,0 +1,82 @@
+<section id="subintro">
+    <div class="jumbotron subhead" id="overview">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="centered">
+              <h3>Pembiayaan Produktif BMT PAS</h3>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+<section id="breadcrumb">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <ul class="breadcrumb notop">
+            <li><a href="index.php">BMT PAS </a><span class="divider">/</span></li>
+            <li class="active">Pembiayaan Produktif BMT Projo Artha Sejahtera</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+	
+</section>
+
+<section id="maincontent">
+    <div class="container">
+      <div class="row">
+
+      <div class="span3">
+          <aside>
+            <div class="widget">
+              <h4>Produk & Layanan Lainnya</h4>
+              <ul class="cat">
+                    <li><a href="index.php?page=tabungan-saham">Tabungan Saham</a></li>
+                    <li><a href="index.php?page=tabungan-qurban">Tabungan Qurban</a></li>
+                    <li><a href="index.php?page=tabungan-thr">Tabungan THR</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Tasbih</a></li>
+                    <li><a href="index.php?page=tabungan-tasbih">Tabungan Simbah</a></li>
+                    <li><a href="index.php?page=pembiayaan-simbah">Pembiayaan Simbah</a></li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+
+        <div class="span9" style="margin-bottom: 60px">
+      		<h4>Apa itu Pembiayaan Produktif? </h4>
+              <p>Merupakan pembiayaan untuk memfasilitasi anggota/nasabah dalam memenuhi kebutuhan investasi atau pengembangan usaha dengan jangka waktu maksimal sampai dengan 36 bulan.</p>
+
+             
+            <h5>Jenis Pembiayan yang di layani :</h5>
+              <ol>
+               
+<li>Modal Usaha</li>
+<li>Dana Sebrakan</li>
+<li>Modal </li>
+<li>Modal Pertanian
+</li>
+              </ol>
+              <h5>Ketentuan Pembiayaan Produktif :</h5>
+              <ol>
+                <li>	Peruntukan/ kegunaan dana BMT PAS adalah untuk memenuhi kebutuhan investasi atau pengembangan usaha
+<li>Angsuran Pembiayaan ada 2 (model) yaitu : <ol><li>
+Pembiayaan dengan angsuran pokok dan keuntungan  yang dibayar setiap bulan </li>
+<li>Pembiayaan dengan angsuran pokok di bayar pada saat jatuh tempo sedangkan keuntungan di bayar setiap bulan </li> </ol>
+<li>Besarnya keuntungan disepakati berdasarkan kesepakatan porsi bagi hasil merujuk pada jenis  usaha, laporan keuangan dan mitigasi resiko bisnisnya</li>
+<li>Jangka Waktu untuk angsuran maximal 36 bulan, dan jangka waktu untuk sebrakan/cash tempo maximal 3 bulan.</li>
+</li>
+              </ol>
+<h5>	Syarat Pembiayaan Produktif :</h5>
+<ol><li>FC KTP Pemohon, suami /istri pemohon</li>
+<li>FC KK dan Akta nikah</li>
+<li>FC Agunan  Sertifikat atau BPKB (Dokumen asli Sertifikat atau BPKB diserahkan pada saat realisasi pembiayaan)</li>
+<li>FC PBB berikut STTSnya jika agunan tanah bangunan, jika kendaraan dilengkapi dengan gesekan nomor mesin & nomor rangka melalui cek bantu Polres/samsat</li>
+<li>FC SIUP, TDP, HO (jika diperlukan)</li>
+
+      </div>
+    </div>
+  </section>
