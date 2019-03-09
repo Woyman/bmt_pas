4b90a50a6887252d740999c19fa53a5b30cc0bd8
16110272 <dwisaputratkj2@gmail.com>
2019-03-09 12:19:17 +0700

    lpz pas content

--- pages/lpz-pas.php
+++ pages/lpz-pas.php
@@ -30,9 +30,49 @@
 	
 </section>
 
+
 <section id="maincontent">
     <div class="container">
       
         <img src="assets/img/lpz-pas_laznas-bsm.jpeg" class="botspacer50 centered" width="800">
+		
+		<p><b>LAZNAS BSM Umat</b> adalah Lembaga Amil Zakat Nasional milik Bank Mandiri Syari’ah, salah satu perbankan syari’ah Nasional terbesar di Indonesia.<p>
+<p><b>Lembaga Pengelola Zakat KSPPS BMT Projo Artha Sejahtera </b>adalah Lembaga Pengelola Zakat yang selanjutnya disingkat LPZ PAS adalah satuan organisasi yang dibentuk oleh LAZNAS BSM Umat untuk membantu pengumpulan zakat di wilayah DIY.</p>
+<h5><b>Pengelolaan zakat,infaq, shodaqoh bertujuan:</b></h5>
+<ol><li>meningkatkan efektivitas dan efisiensi pelayanan dalam pengelolaan zakat, infaq dan shodaqoh</li>
+<li>meningkatkan manfaat zakat, infaq, shodaqoh untuk mewujudkan kesejahteraan masyarakat dan penanggulangan kemiskinan.</li></ol>
+<h5>Apa Saja Program Sosial LPZ PAS itu?</h5>
+<ol><li>Program Ibu Mengaji untuk Maju Mandiri (PIM3)
+Merupakan program pemberdayaan khusus ibu-ibu dhuafa untuk memajukan kesejahteraan perekonomian dan kerohanian.
+Kegiatan Program PIM3 bersifat pekanan (pertemuan 1 x seminggu) ini diantaranya :
+<ol><li>Pembelajaran baca Al-qur’an</li>
+<li>Pinjaman tanpa jasa (kembali pokok saja)</li>
+<li>Pemberdayaan ekonomi</li>
+<li>Bimbingan Keagamaan</li></ol></li>
+<li>Santunan Anak Yatim</li>
+<li>Cek Kesehatan kaum dhuafa (gratis) berupa : diagnose umum, cek gula, asam urat, collesterol</li>
+<li>Pengajian Sabtu kliwon</li>
+<li>Pengadaan mobil Ambulance*</li>
+
+<h5>Pengertian dan siapa saja yang wajib Zakat, infaq dan shodaqoh?</h5>
+<ol><li>Zakat adalah harta yang wajib dikeluarkan oleh seorang muslim atau badan usaha yang telah mencapai syarat tertentu (nishab) untuk diberikan kepada yang berhak menerimanya sesuai dengan syariat Islam.
+<ol type="a"><li>emas, perak, dan logam mulia lainnya;</li>
+<li>uang dan surat berharga lainnya;</li>
+<li>perniagaan;</li>
+<li>pertanian, perkebunan, dan kehutanan;</li>
+<li>peternakan dan perikanan</li>
+<li>pertambangan;</li>
+<li>perindustrian;</li>
+<li>pendapatan dan jasa; dan</li>
+<li>rikaz</li></ol></li>
+<li>Infak adalah harta yang dikeluarkan oleh seseorang atau badan usaha di luar zakat untuk kemaslahatan umum. Pemberi Infaq  boleh siapapun baik yang berpenghasilan tinggi maupun rendah, apakah ia di saat lapang maupun sempit.</li>
+<li>Sedekah adalah harta atau non harta yang dikeluarkan oleh seseorang atau badan usaha di luar zakat untuk kemaslahatan umum.</li>
+
+
+
+
+		
+		
+		
     </div>
   </section>
