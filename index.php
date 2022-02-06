1019d0617784163bfebfb8a88f99b342dd9e237d
Woyman <woyman11@gmail.com>
2022-02-07 02:01:13 +0700

    test server variable

--- index.php
+++ index.php
@@ -1,39 +1,40 @@
 <?php
-ob_start();
-session_start();
-error_reporting(E_ALL);
-/*setlocale(LC_ALL, 'Indonesia');
-date_default_timezone_set('Asia/Jakarta');*/
-		include("inc/config/connection.php");
-		include("inc/function.php");
-		include("inc/variable.php");		
+var_export($_SERVER); die();
+// ob_start();
+// session_start();
+// error_reporting(E_ALL);
+// /*setlocale(LC_ALL, 'Indonesia');
+// date_default_timezone_set('Asia/Jakarta');*/
+// 		include("inc/config/connection.php");
+// 		include("inc/function.php");
+// 		include("inc/variable.php");		
 		
-		$qGetInfoPengunjungVariable = "SELECT * FROM setting WHERE id_setting = '1' ";
-		$getInfoPengunjungVariable = mysqli_query($connect, $qGetInfoPengunjungVariable);
-		$infoPengunjungVariable = mysqli_fetch_array($getInfoPengunjungVariable);
-		$pengunjung = $infoPengunjungVariable['jml_pengunjung'];
+// 		$qGetInfoPengunjungVariable = "SELECT * FROM setting WHERE id_setting = '1' ";
+// 		$getInfoPengunjungVariable = mysqli_query($connect, $qGetInfoPengunjungVariable);
+// 		$infoPengunjungVariable = mysqli_fetch_array($getInfoPengunjungVariable);
+// 		$pengunjung = $infoPengunjungVariable['jml_pengunjung'];
 
-			if (!$page){
-					header("location: index.php?page=home");
-			}
-			 else  if(!file_exists('pages/'.$page.'.php')) {
-			 	include("pages/404.php");
-			}
-			elseif($page== 'login')
-			{
-				include('pages/login.php');
-			}
-			else { 
-				include("layout/header.php");
-				include("layout/nav.php");
-					if($page=='home')
-					{
-						include("layout/cover.php");
-					} 
-				include("pages/$page.php"); 	 
-				include("layout/footer.php");	
-				include("layout/script.php");	
-			}
+// 			if (!$page){
+// 					header("location: index.php?page=home");
+// 			}
+// 			 else  if(!file_exists('pages/'.$page.'.php')) {
+// 			 	include("pages/404.php");
+// 			}
+// 			elseif($page== 'login')
+// 			{
+// 				include('pages/login.php');
+// 			}
+// 			else { 
+// 				include("layout/header.php");
+// 				include("layout/nav.php");
+// 					if($page=='home')
+// 					{
+// 						include("layout/cover.php");
+// 					} 
+// 				include("pages/$page.php"); 	 
+// 				include("layout/footer.php");	
+// 				include("layout/script.php");	
+// 			}
 ?>
  </body>
 </html>
