c25cccd2eb2509cce065ac38eb8ab0392b86669c
Woyman <woyman11@gmail.com>
2022-02-11 01:33:31 +0700

    change navbar href

--- layout/nav.php
+++ layout/nav.php
@@ -16,60 +16,60 @@
               </button>
                 <ul class="nav topnav">
                   <li <?php if($page == 'home' ){echo "class='active' ";} ?> >
-                    <a href="home">HOME</a>
+                    <a href="index.php?page=home">HOME</a>
                   </li>
                   <li>
-                    <a href="berita">BERITA</a>
+                    <a href="index.php?page=berita">BERITA</a>
                   </li>
                   <li class="dropdown 
                     <?php if( $page == 'pembiayaan-konsumtif' ||$page == 'pembiayaan-produktif' || $page == 'pembiayaan-multiguna' || $page == 'pembiayaan-gadai' || $page == 'tabungan-saham' || $page == 'tabungan-qurban' || $page == 'tabungan-thr' || $page == 'tabungan-tasbih' || $page == 'tabungan-smart' || $page == 'tabungan-ibadah' || $page == 'ppob-absindo'  )
                     {echo "active ";} ?> ?>
                   ">
-                    <a href="#">PRODUK & LAYANAN</a>
+                    <a href="index.php?page=#">PRODUK & LAYANAN</a>
                     <ul class="dropdown-menu">
-            					<li class="dropdown"><a href="#">Pembiayaan <i class="icon-chevron-right" style="float: right; margin-top: 5px"></i></a>
+            					<li class="dropdown"><a href="index.php?page=#">Pembiayaan <i class="icon-chevron-right" style="float: right; margin-top: 5px"></i></a>
                          <ul class="dropdown-menu sub-menu">
-                          <li><a href="pembiayaan-produktif">Pembiayaan Produktif</a></li>
-                          <li><a href="pembiayaan-konsumtif">Pembiayaan Konsumtif</a></li>
-                          <li><a href="pembiayaan-multiguna">Pembiayaan Multiguna</a></li>
-                          <li><a href="pembiayaan-gadai">Pembiayaan Gadai</a></li>
+                          <li><a href="index.php?page=pembiayaan-produktif">Pembiayaan Produktif</a></li>
+                          <li><a href="index.php?page=pembiayaan-konsumtif">Pembiayaan Konsumtif</a></li>
+                          <li><a href="index.php?page=pembiayaan-multiguna">Pembiayaan Multiguna</a></li>
+                          <li><a href="index.php?page=pembiayaan-gadai">Pembiayaan Gadai</a></li>
                         </ul>
                       </li>
-                      <li class="dropdown"><a href="#">Tabungan <i class="icon-chevron-right" style="float: right; margin-top: 5px"></i></a>
+                      <li class="dropdown"><a href="index.php?page=#">Tabungan <i class="icon-chevron-right" style="float: right; margin-top: 5px"></i></a>
                         <ul class="dropdown-menu sub-menu">
-                          <li><a href="tabungan-saham">Tabungan Saham</a></li>
-                          <li><a href="tabungan-qurban">Tabungan Qurban</a></li>
-                          <li><a href="tabungan-thr">Tabungan THR</a></li>
-                          <li><a href="tabungan-tasbih">Tabungan Tasbih</a></li>
-                          <li><a href="tabungan-simbah">Tabungan Simbah</a></li>
-                          <li><a href="tabungan-ibadah">Tabungan Ibadah</a></li>
-                          <li><a href="tabungan-smart">Tabungan Smart</a></li>
+                          <li><a href="index.php?page=tabungan-saham">Tabungan Saham</a></li>
+                          <li><a href="index.php?page=tabungan-qurban">Tabungan Qurban</a></li>
+                          <li><a href="index.php?page=tabungan-thr">Tabungan THR</a></li>
+                          <li><a href="index.php?page=tabungan-tasbih">Tabungan Tasbih</a></li>
+                          <li><a href="index.php?page=tabungan-simbah">Tabungan Simbah</a></li>
+                          <li><a href="index.php?page=tabungan-ibadah">Tabungan Ibadah</a></li>
+                          <li><a href="index.php?page=tabungan-smart">Tabungan Smart</a></li>
                         </ul>
                       </li>
-                      <li><a href="ppob-absindo">Pembayaran Online</a></li>
+                      <li><a href="index.php?page=ppob-absindo">Pembayaran Online</a></li>
                     </ul>
                   </li>
-                  <li <?php if($page == 'lpz-pas' ){echo "class='active' ";} ?> ><a href="lpz-pas">LPZ PAS - LAZNAS BSM</a></li>
+                  <li <?php if($page == 'lpz-pas' ){echo "class='active' ";} ?> ><a href="index.php?page=lpz-pas">LPZ PAS - LAZNAS DEWAN DAKWAH</a></li>
                   <li <?php if($page == 'gallery' ){echo "class='active' ";} ?>>
-                    <a href="gallery">GALERI</a>
+                    <a href="index.php?page=gallery">GALERI</a>
                   </li>
                   <li class='dropdown <?php if($page == 'profile' ){echo "active ";} ?>'  >                    
-                    <a href="#">TENTANG KAMI</a>
+                    <a href="index.php?page=#">TENTANG KAMI</a>
                       <ul class="dropdown-menu">                        
-                        <li><a href="profil">Profil</a></li>
-                        <li><a href="manajemen">Manajemen</a></li>
-                        <li><a href="sejarah">Sejarah</a></li>
-                        <li><a href="legalitas">Legalitas Kelembagaan</a></li>
+                        <li><a href="index.php?page=profil">Profil</a></li>
+                        <li><a href="index.php?page=manajemen">Manajemen</a></li>
+                        <li><a href="index.php?page=sejarah">Sejarah</a></li>
+                        <li><a href="index.php?page=legalitas">Legalitas Kelembagaan</a></li>
                       </ul>
                   </li>
                   <li <?php if($page == 'hubungi-kami' ){echo "class='active' ";} ?>>
-                    <a href="hubungi-kami">HUBUNGI KAMI</a>
+                    <a href="index.php?page=hubungi-kami">HUBUNGI KAMI</a>
                   </li>
                   <li class="dropdown hover" >
-                    <a href="#" style="color: #000;"><i class="icon-calendar icon-rounded"></i>Simulasi Perhitungan</a>
+                    <a href="index.php?page=#" style="color: #000;"><i class="icon-calendar icon-rounded"></i>Simulasi Perhitungan</a>
                     <ul class="dropdown-menu">
-                      <li><a href="simulasi" style="color: #000;"></i>Simulasi Pembiayaan</a></li>
-                      <li><a href="kalkulator-zakat" style="color: #000;">Kalkulator Zakat</a>
+                      <li><a href="index.php?page=simulasi" style="color: #000;"></i>Simulasi Pembiayaan</a></li>
+                      <li><a href="index.php?page=kalkulator-zakat" style="color: #000;">Kalkulator Zakat</a>
                       </li>
                     </ul>
                   </li>
@@ -79,62 +79,62 @@
             </div>
           
               <div class="sidenav" id="sidenav">
-                <a href="javascript:void(0)" class="closebtn" onclick="toogleNav()">&times;</a>
+                <a href="index.php?page=javascript:void(0)" class="closebtn" onclick="toogleNav()">&times;</a>
                 <ul id="list-menu-sidebar">
                   <li>
-                    <a href="home">HOME</a>
+                    <a href="index.php?page=home">HOME</a>
                   </li>
                   <li>
-                    <a href="berita">BERITA</a>
+                    <a href="index.php?page=berita">BERITA</a>
                   </li>
                   <li class="dropdown">
-                    <a href="#">PRODUK & LAYANAN <i class="icon-chevron-down dropdown-icon"></i> </a>
+                    <a href="index.php?page=#">PRODUK & LAYANAN <i class="icon-chevron-down dropdown-icon"></i> </a>
                     <ul class="dropdown-container">
             					<li class="dropdown">
-                          <a href="#">Pembiayaan <i class="icon-chevron-down dropdown-icon"></i></a>
+                          <a href="index.php?page=#">Pembiayaan <i class="icon-chevron-down dropdown-icon"></i></a>
                           <ul class="dropdown-container">
-                            <li><a href="pembiayaan-produktif">Pembiayaan Produktif</a></li>
-                            <li><a href="pembiayaan-konsumtif">Pembiayaan Konsumtif</a></li>
-                            <li><a href="pembiayaan-multiguna">Pembiayaan Multiguna</a></li>
-                            <li><a href="pembiayaan-gadai">Pembiayaan Gadai</a></li>
+                            <li><a href="index.php?page=pembiayaan-produktif">Pembiayaan Produktif</a></li>
+                            <li><a href="index.php?page=pembiayaan-konsumtif">Pembiayaan Konsumtif</a></li>
+                            <li><a href="index.php?page=pembiayaan-multiguna">Pembiayaan Multiguna</a></li>
+                            <li><a href="index.php?page=pembiayaan-gadai">Pembiayaan Gadai</a></li>
                         </ul>
                       </li>
                       <li class="dropdown">
-                        <a href="#">Tabungan <i class="icon-chevron-down dropdown-icon"></i></a>
+                        <a href="index.php?page=#">Tabungan <i class="icon-chevron-down dropdown-icon"></i></a>
                         <ul class="dropdown-container ">
-                          <li><a href="tabungan-saham">Tabungan Saham</a></li>
-                          <li><a href="tabungan-qurban">Tabungan Qurban</a></li>
-                          <li><a href="tabungan-thr">Tabungan THR</a></li>
-                          <li><a href="tabungan-tasbih">Tabungan Tasbih</a></li>
-                          <li><a href="tabungan-simbah">Tabungan Simbah</a></li>
-                          <li><a href="tabungan-ibadah">Tabungan Ibadah</a></li>
-                          <li><a href="tabungan-smart">Tabungan Smart</a></li>
+                          <li><a href="index.php?page=tabungan-saham">Tabungan Saham</a></li>
+                          <li><a href="index.php?page=tabungan-qurban">Tabungan Qurban</a></li>
+                          <li><a href="index.php?page=tabungan-thr">Tabungan THR</a></li>
+                          <li><a href="index.php?page=tabungan-tasbih">Tabungan Tasbih</a></li>
+                          <li><a href="index.php?page=tabungan-simbah">Tabungan Simbah</a></li>
+                          <li><a href="index.php?page=tabungan-ibadah">Tabungan Ibadah</a></li>
+                          <li><a href="index.php?page=tabungan-smart">Tabungan Smart</a></li>
                         </ul>
                       </li>
-                      <li><a href="ppob-absindo">Pembayaran Online</a></li>
+                      <li><a href="index.php?page=ppob-absindo">Pembayaran Online</a></li>
                     </ul>
                   </li>
-                  <li><a href="lpz-pas">LPZ PAS - LAZNAS BSM</a></li>
+                  <li><a href="index.php?page=lpz-pas">LPZ PAS - LAZNAS BSM</a></li>
                   <li>
-                    <a href="gallery">GALERI</a>
+                    <a href="index.php?page=gallery">GALERI</a>
                   </li>
                   <li class='dropdown' >                    
-                      <a href="#">TENTANG KAMI <i class="icon-chevron-down dropdown-icon"></i> </a>
+                      <a href="index.php?page=#">TENTANG KAMI <i class="icon-chevron-down dropdown-icon"></i> </a>
                       <ul class="dropdown-container">                        
-                        <li><a href="profil">Profil</a></li>
-                        <li><a href="manajemen">Manajemen</a></li>
-                        <li><a href="sejarah">Sejarah</a></li>
-                        <li><a href="legalitas">Legalitas Kelembagaan</a></li>
+                        <li><a href="index.php?page=profil">Profil</a></li>
+                        <li><a href="index.php?page=manajemen">Manajemen</a></li>
+                        <li><a href="index.php?page=sejarah">Sejarah</a></li>
+                        <li><a href="index.php?page=legalitas">Legalitas Kelembagaan</a></li>
                       </ul>
                   </li>
                   <li >
-                    <a href="hubungi-kami">HUBUNGI KAMI</a>
+                    <a href="index.php?page=hubungi-kami">HUBUNGI KAMI</a>
                   </li>
                   <li class="dropdown" style = "background-color: #b71200" >
-                    <a href="#"><i class="icon-calendar icon-rounded"></i>Simulasi Perhitungan</a>
+                    <a href="index.php?page=#"><i class="icon-calendar icon-rounded"></i>Simulasi Perhitungan</a>
                     <ul class="dropdown-container">
-                      <li><a href="simulasi" ></i>Simulasi Pembiayaan</a></li>
-                      <li><a href="kalkulator-zakat" >Kalkulator Zakat</a>
+                      <li><a href="index.php?page=simulasi" ></i>Simulasi Pembiayaan</a></li>
+                      <li><a href="index.php?page=kalkulator-zakat" >Kalkulator Zakat</a>
                       </li>
                     </ul>
                   </li>
@@ -165,7 +165,6 @@
 
       for (i = 0; i < dropdown.length; i++) {
         
-        console.log(dropdown[i].firstElementChild)
         if(dropdown[i].firstElementChild.tagName == 'A') {
           dropdown[i].firstElementChild.addEventListener("click", function() {
             this.classList.toggle("active");
@@ -176,8 +175,7 @@
             } else {
               dropdownContent.style.display = "block";
             }
-          
-            
+                      
           });
         }
 
